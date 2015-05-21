integral constraint on phi is fixed, error comparison of pressure gradient is added

structure2.py
@@ -35,9 +35,10 @@ class mesh:
         self.tdomain = time_domain
         self.CFL = CFL
         # dt: delta t
-        self.dt = abs(((self.sdomain[0][1] - self.sdomain[0][0])/self.gds[0])*CFL)
+        self.dt1 = abs(((self.sdomain[0][1] - self.sdomain[0][0])/self.gds[0])*CFL)
         # tn: number of iterations
-        self.Tn = int(round(self.tdomain[1]/self.dt))
+        self.Tn = int(round(self.tdomain[1]/self.dt1))
+	self.dt = abs(float(self.tdomain[1] - self.tdomain[0]))/self.Tn
         # dx, dy: delta x and delta y
         self.dx = abs(float(self.sdomain[0][1] - self.sdomain[0][0]))/self.n
         self.dy = abs(float(self.sdomain[1][1] - self.sdomain[1][0]))/self.m
@@ -592,6 +593,10 @@ class Exact_solutions():
         self.Yvbnd = mesh.vbndmg("y")
         self.XPint = mesh.pintmg("x")
         self.YPint = mesh.pintmg("y")
+	self.Xuint = mesh.uintmg("x")
+	self.Yuint = mesh.uintmg("y")
+	self.Xvint = mesh.vintmg("x")
+	self.Yvint = mesh.vintmg("y")
         
     def Exact_solutions(self, Solution_type):
         dt = self.mesh.dt
@@ -600,26 +605,31 @@ class Exact_solutions():
         Xubnd, Yubnd = self.Xubnd, self.Yubnd
         Xvbnd, Yvbnd = self.Xvbnd, self.Yvbnd
         XPint, YPint = self.XPint, self.YPint
-        
+        Xuint, Yuint = self.Xuint, self.Yuint
+        Xvint, Yvint = self.Xvint, self.Yvint       
+
         tn = dt*t + self.mesh.tdomain[0]
 	tnhalf = dt*(t-0.5) + self.mesh.tdomain[0]
 
         if Solution_type == "Taylor":
             U_exact_bnd = -np.cos(Xubnd)*np.sin(Yubnd)*np.exp(-2*tn)
             V_exact_bnd = np.sin(Xvbnd)*np.cos(Yvbnd)*np.exp(-2*tn)
-            P_exact = -Re*0.25*(np.cos(2*XPint) + np.cos(2*YPint))*np.exp(-4*tnhalf)
-
+            P_exact = -(1/(4*Re))*(np.cos(2*XPint) + np.cos(2*YPint))*np.exp(-4*tnhalf)
+	    gradpu_exact = (1/(2*Re))*np.sin(2*Xuint)*np.exp(-4*tnhalf)
+	    gradpv_exact = (1/(2*Re))*np.sin(2*Yvint)*np.exp(-4*tnhalf)
 
 	elif Solution_type == 'periodic_forcing_1':
 	    U_exact_bnd = np.pi*np.sin(tn)*np.sin(2*np.pi*Yubnd)*(np.sin(np.pi*Xubnd)**2)
 	    V_exact_bnd = -np.pi*np.sin(tn)*np.sin(2*np.pi*Xvbnd)*(np.sin(np.pi*Yvbnd)**2)
 	    P_exact = np.sin(tnhalf)*np.sin(np.pi*YPint)*np.cos(np.pi*XPint)
-											            
-	return VelocityField(U_exact_bnd, V_exact_bnd, self.mesh), CentredPotential(P_exact, self.mesh)
+	    gradpu_exact = -np.pi*np.sin(tnhalf)*np.sin(np.pi*Xuint)*np.sin(np.pi*Yuint)
+	    gradpv_exact = np.pi*np.sin(tnhalf)*np.cos(np.pi*Xvint)*np.cos(np.pi*Yvint)
+
+	return VelocityField(U_exact_bnd, V_exact_bnd, self.mesh), CentredPotential(P_exact, self.mesh), VelocityField(gradpu_exact, gradpv_exact, self.mesh)
  
 class Forcing_term:
     '''This class contains the external forcing term that are required for some flow problems e.g periodic_forcing_1'''
-    def __init__(self, mesh, t):
+    def __init__(self, mesh, test_problem_name, t):
         self.n = mesh.n
         self.m = mesh.m
         self.xu = mesh.xu
@@ -635,36 +645,46 @@ class Forcing_term:
         self.dx = mesh.dx
         self.dy = mesh.dy
         self.t = t
+	self.test_problem_name = test_problem_name
 	self.Re = mesh.Re
 	
-    def periodic_forcing_1(self, t):
+    def periodic_forcing_1(self):
+        print 'periodic_forcing_1'
         n = self.n
         m = self.m
         dx = self.dx
         dy = self.dy
         dt = self.dt
+	t = self.t
         Re = self.Re
 	# change all Xu and Xv, Yu, Yv into xu, xv, yu, yv
         Xu, Yu = np.meshgrid(self.xu, self.yu)
         Xv, Yv = np.meshgrid(self.xv, self.yv)
-        
+#        print t, dt, self.tdomain[0]
+#	print np.sin(2*np.pi*Yu)
+#	print np.pi*np.cos(tn)
+#	print Yu
+
         tn = dt*t + self.tdomain[0]
         # these forcing terms include boundary points
         Fx = np.pi*np.cos(tn)*np.sin(2*np.pi*Yu)*(np.sin(np.pi*Xu)**2) -\
              2*(np.pi**3)*np.sin(tn)*np.sin(2*np.pi*Yu)*(np.cos(2*np.pi*Xu) - 2*(np.sin(np.pi*Xu)**2)) -\
              np.pi*np.sin(tn)*np.sin(np.pi*Yu)*np.sin(np.pi*Xu)
+
         Fy = -np.pi*np.cos(tn)*np.sin(2*np.pi*Xv)*(np.sin(np.pi*Yv)**2) -\
              2*(np.pi**3)*np.sin(tn)*np.sin(2*np.pi*Xv)*(2*(np.sin(np.pi*Yv)**2) - np.cos(2*np.pi*Yv)) +\
              np.pi*np.sin(tn)*np.cos(np.pi*Xv)*np.cos(np.pi*Yv)
-
+#	print Fx, 'Fx'
+#	print Fy, 'Fy'
         return [Fx[:,1:n], Fy[1:m,:]]
 
-    def periodic_forcing_2(self, t):
+    def periodic_forcing_2(self): 
         n = self.n
         m = self.m
         dx = self.dx
         dy = self.dy
         dt = self.dt
+	t = self.t
         Re = self.Re
         Xu, Yu = np.meshgrid(self.xu, self.yu)
         Xv, Yv = np.meshgrid(self.xv, self.yv)      
@@ -678,9 +698,9 @@ class Forcing_term:
         
         return [Fx[:,1:n], Fy[1:m,:]]
 
-    def select_forcing_term(self, test_problem_name, t):
-	if test_problem_name == 'periodic_forcing_1':
-	    forcing_term = self.periodic_forcing_1(t)
+    def select_forcing_term(self):
+	if self.test_problem_name == 'periodic_forcing_1':
+	    forcing_term = self.periodic_forcing_1()
 	else:
 	    forcing_term = 0
 	return forcing_term

test2.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 29 22:26:38 2014
-
-@author: acerlinux
-"""
-
+from __future__ import division
 import sys
 from mpl_toolkits.mplot3d import *
 import numpy as np
@@ -88,7 +83,7 @@ def get_inputs():
 		plot_option = True
 	elif plot_option == '':
 		# take default
-		plot_option = True
+		plot_option = False
 	else:
 		plot_option = False
 
@@ -103,6 +98,8 @@ def run_Navier_Stokes_solver(xl, xr, t0, tf, gridsize, method, test_problem_name
 	mesh = structure2.mesh(grid_size_domain,spatial_domain,time_domain,CFL,Re)
 	print mesh.dx, "dx"
 	print mesh.dt, "dt"
+	print spatial_domain, 'spatial domain'
+	print time_domain, 'time domain'
 	Xubnd = mesh.ubndmg("x")
 	Yubnd = mesh.ubndmg("y")
 	Xvbnd = mesh.vbndmg("x")
@@ -118,7 +115,7 @@ def run_Navier_Stokes_solver(xl, xr, t0, tf, gridsize, method, test_problem_name
 		# initial set up
 		init_setup = Gauge.setup(ic_uv_init, test_problem_name)
 		# iterative solve process
-		uvf_cmp, pf = Gauge.iterative_solver(test_problem_name, mesh.Tn, init_setup)
+		uvf_cmp, pf, gradp = Gauge.iterative_solver(test_problem_name, mesh.Tn, init_setup)
 #		print pf.get_value()
 
 	elif method == 'Alg1':
@@ -128,7 +125,7 @@ def run_Navier_Stokes_solver(xl, xr, t0, tf, gridsize, method, test_problem_name
 		# initial set up
 		init_setup = Alg1.setup(ic_init, test_problem_name)
 		# iterative solve process
-		uvf_cmp, pf = Alg1.iterative_solver(test_problem_name, mesh.Tn, init_setup)
+		uvf_cmp, pf, gradp = Alg1.iterative_solver(test_problem_name, mesh.Tn, init_setup)
 #		print uvf_cmp.get_uv(), pf.get_value()
 	
 	elif method == 'Alg2':
@@ -138,7 +135,7 @@ def run_Navier_Stokes_solver(xl, xr, t0, tf, gridsize, method, test_problem_name
 		# initial set up
 		init_setup = Alg2.setup(ic_uv_init, test_problem_name)
 		# iterative solve process
-		uvf_cmp, pf = Alg2.iterative_solver(test_problem_name, mesh.Tn, init_setup)
+		uvf_cmp, pf, gradp = Alg2.iterative_solver(test_problem_name, mesh.Tn, init_setup)
 #		print pf.get_value()
 	
 	# comparison and error analysis
@@ -146,15 +143,28 @@ def run_Navier_Stokes_solver(xl, xr, t0, tf, gridsize, method, test_problem_name
 		# no analytical solutions available
 		pass
 	else:
-		uv_exact_bnd, p_exact = structure2.Exact_solutions(mesh, Re, mesh.Tn).Exact_solutions(test_problem_name)
+		uv_exact_bnd, p_exact, gradp_exact = structure2.Exact_solutions(mesh, Re, mesh.Tn).Exact_solutions(test_problem_name)
 		div_uvf = uvf_cmp.divergence()
-		
-		Error = solvers2.Error(uvf_cmp, uv_exact_bnd, pf, p_exact, div_uvf, mesh)
+		print np.sum(p_exact.get_value()), 'integral of exact pressure'
+#		print p_exact.get_value(), 'exact pressure'
+#		pc = np.sum(p_exact.get_value())
+#		area_domain = (spatial_domain[0][1] - spatial_domain[0][0])**2
+#		d = pc/area_domain
+#		print area_domain, 'area of domain'
+#		print d, 'constant'
+#		p_exact = p_exact - pc
+#		print p_exact.get_value(), 'exact pressure normalised'
+#		print np.sum(p_exact.get_value()), 'integral of exact pressure normalised' 	
+		Error = solvers2.Error(uvf_cmp, uv_exact_bnd, pf, p_exact, gradp, gradp_exact, div_uvf, mesh)
 		Velocity_error = Error.velocity_error()
 		Pressure_error = Error.pressure_error()
+		gradpu_error, gradpv_error, avg_gradp_error = Error.pressure_gradient_error()
 		print "U velocity error is %s " % Velocity_error[0]
 		print "V velocity error is %s " % Velocity_error[1]
 		print "Pressure error is %s " % Pressure_error
+		print "gradient P u error is %s " % gradpu_error
+		print "gradient P v error is %s " % gradpv_error
+		print "average gradient Pressure error is %s " % avg_gradp_error
 	
 	if plot_option == False:
 		return None